bbb93bdccc47f57abab2f33742a1f781c30f4cd9
hardeywhaley <adedokunmuhammed2022@gmail.com>
2026-05-12 22:22:03 +0100

    fix welcom

--- public/login.js
+++ public/login.js
@@ -21,7 +21,7 @@ form.addEventListener("submit", function (e) {
         errorMessage.textContent = "";
 
         // Redirect to quiz page
-        window.location.href = "/public/index.html";
+        window.location.href = "/public/quiz.html";
     } else {
         // Show error message
         errorMessage.textContent =
